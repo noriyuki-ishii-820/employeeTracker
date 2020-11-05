c8333b7dee2e8a6ec548234a5e9f9b7a809fbb42
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-11-05 16:54:28 +1100

    add dept done

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const inquirer = require("inquirer");
 var mysql = require("mysql");
 const cTable = require("console.table");
 
+var roleArray = [];
+var managerArray = [];
+
 var connection = mysql.createConnection({
   host: "localhost",
 
@@ -83,8 +86,8 @@ function viewEmployees() {
   connection.query(query, function (err, res) {
     if (err) throw err;
     console.table(res);
-    startSearch();
   });
+  startSearch();
 }
 
 function viewEmployeesByDept() {
@@ -120,7 +123,6 @@ function viewEmployeesByManager() {
   const query =
     "SELECT CONCAT(employee.first_name, ' ', employee.last_name) AS name FROM employee LEFT JOIN role ON employee.role_id = role.id  WHERE role.title LIKE '%Manager' OR '%manager'";
   connection.query(query, function (err, res) {
-    console.log(res);
     if (err) throw err;
 
     inquirer
@@ -133,18 +135,98 @@ function viewEmployeesByManager() {
         },
       ])
       .then(function (answer) {
-        console.log(answer);
         const query2 =
           "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id WHERE department.id = manager_id;";
 
         connection.query(query2, [answer.choices], function (err, res) {
-          console.log(res);
           if (err) throw err;
           console.table(res);
           startSearch();
         });
       });
   });
+}
+
+function findRole() {
+  const query = "SELECT title FROM role";
+  connection.query(query, function (err, results) {
+    if (err) throw err;
+    for (var i = 0; i < results.length; i++) {
+      roleArray.push(results[i].title);
+    }
+  });
+  return roleArray;
+}
+
+function findManager() {
+  const query =
+    "SELECT CONCAT(first_name, ' ', last_name) AS manager FROM employee WHERE manager_id IS null";
+  connection.query(query, function (err, results) {
+    if (err) throw err;
+    for (var i = 0; i < results.length; i++) {
+      managerArray.push(results[i].manager);
+    }
+  });
+  return managerArray;
+}
+
+// need to take another look
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: "firstName",
+        type: "input",
+        message: "What is the first name of the new employee?",
+      },
+      {
+        name: "lastName",
+        type: "input",
+        message: "What is the last name of the new employee?",
+      },
+      {
+        name: "role",
+        type: "list",
+        message: "What is the role of the new employee?",
+        choices: findRole(),
+      },
+      {
+        name: "manager",
+        type: "list",
+        choices: findManager(),
+        message: "Who is the manager of the new employee?",
+      },
+    ])
+    .then(function (answer) {
+      var roleId = findRole().indexOf(answer.role) + 1;
+      var managerId = findManager().indexOf(answer.manager) + 1;
+      connection.query("INSERT INTO employee SET ?", {
+        first_name: answer.firstName,
+        last_name: answer.lastName,
+        role_id: roleId,
+        manager_id: managerId,
+      });
+    });
+
+  startSearch();
+}
+
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        name: "deptName",
+        type: "input",
+        message: "What is the name of the new Department?",
+      },
+    ])
+    .then(function (answer) {
+      connection.query("INSERT INTO department SET ?", {
+        name: answer.deptName,
+      });
+      console.log("Added the new " + answer.deptName + " department.");
+    });
+  startSearch();
 }
 
 startSearch();
